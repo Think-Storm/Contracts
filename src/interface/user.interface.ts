de0bd837d771f5539165ef6b74ab1a8ae7103a00
kimdahee0815 <billionairedahee@gmail.com>
2025-05-23 23:27:49 +0900

    feat: remove password from updateuser and add changepasswordat to userresponse

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -1,28 +1,27 @@
-
 export interface UpdateUser {
-  id: number;
-  username: string;
-  email: string;
-  password: string;
-  fullName: string;
-  birthdate: Date;
+    id: number;
+    username: string;
+    email: string;
+    fullName: string;
+    birthdate: Date;
 }
 
 export interface CreateUser {
-  username: string;
-  email: string;
-  password: string;
-  fullName: string;
-  birthdate: Date;
+    username: string;
+    email: string;
+    password: string;
+    fullName: string;
+    birthdate: Date;
 }
 
 export interface UserResponse {
-  id: number;
-  email: string;
-  username: string;
-  password: string;
-  fullName?: string;
-  birthdate?: Date;
-  createdAt: Date;
-  lastUpdatedAt: Date;
+    id: number;
+    email: string;
+    username: string;
+    password: string;
+    passwordChangedAt?: Date;
+    fullName?: string;
+    birthdate?: Date;
+    createdAt: Date;
+    lastUpdatedAt: Date;
 }
