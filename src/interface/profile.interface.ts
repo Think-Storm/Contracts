196ae2253393959e02b8869fb9d9655be862a3a5
kimdahee0815 <billionairedahee@gmail.com>
2025-05-28 16:37:16 +0900

    fix: create profile preferred_role string array

--- src/interface/profile.interface.ts
+++ src/interface/profile.interface.ts
@@ -1,28 +1,28 @@
-import { LanguageCode } from '../common/types/language.types';
-import { UserRole } from '../common/types/user.types';
+import { LanguageCode } from "../common/types/language.types";
+import { UserRole } from "../common/types/user.types";
 
 export interface CreateProfile {
-  avatar?: string;
-  bio?: string;
-  preferred_role?: string[];
-  location?: string;
-  website?: string;
-  domain_labels?: string[];
-  languages?: LanguageCode[];
-  technical_labels?: string[];
+    avatar?: string;
+    bio?: string;
+    preferred_role?: UserRole[];
+    location?: string;
+    website?: string;
+    domain_labels?: string[];
+    languages?: LanguageCode[];
+    technical_labels?: string[];
 }
 
 export interface ProfileResponse {
-  id: number;
-  userId: number;
-  avatar?: string;
-  bio?: string;
-  preferred_role?: UserRole[];
-  location?: string;
-  website?: string;
-  domain_labels?: string[];
-  languages?: LanguageCode[];
-  technical_labels?: string[];
-  createdAt: Date;
-  lastUpdatedAt: Date;
+    id: number;
+    userId: number;
+    avatar?: string;
+    bio?: string;
+    preferred_role?: UserRole[];
+    location?: string;
+    website?: string;
+    domain_labels?: string[];
+    languages?: LanguageCode[];
+    technical_labels?: string[];
+    createdAt: Date;
+    lastUpdatedAt: Date;
 }
