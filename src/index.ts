5e48bbe9bd449b119664326dd3a0f8c39f6ff797
kimdahee0815 <billionairedahee@gmail.com>
2025-07-03 08:53:16 +0900

    fix: backend dtos moved to contracts

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,69 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 
-// Auth 
+// Auth
 
-export { 
-     LoginUser, 
-     RegisterUser 
-} from './interface/auth.interface';
+export { LoginUser, RegisterUser, UpdatePassword, ForgotPassword } from "./interface/auth.interface";
 
 // User
 
-export { 
-     CreateUser,
-     UpdateUser,
-     UserResponse
-} from './interface/user.interface';
+export { CreateUser, UpdateUser, UserResponse } from "./interface/user.interface";
 
 // Project
 
-export { 
-     CreateProject,
-     UpdateProject,
-     ProjectResponse,
-     SearchProject,
-     GetProject,
-     SearchProjectResponse,
-     ProjectJoinRequest,
- } from './interface/project.interface';
+export {
+    CreateProject,
+    UpdateProject,
+    GetProject,
+    ProjectResponse,
+    SearchProject,
+    JoinRequestResponse,
+    SearchProjectResponse,
+    CreateJoinRequest,
+    Saveproject,
+} from "./interface/project.interface";
 
 // Newsletter
 
-export { 
-     CreateNewsletterSubscription,
-     NewsletterSubscriptionResponse,
- } from './interface/newsletter.interface';
+export { CreateNewsletterSubscription, NewsletterSubscriptionResponse } from "./interface/newsletter.interface";
 
 // Notification
-export { 
-     CreateNotification,
-     SetNotificationRead,
-     NotificationResponse,
- } from './interface/notification.interface';
+export { CreateNotification, SetNotificationRead, NotificationResponse } from "./interface/notification.interface";
 
 // Profile
-export { 
-     CreateProfile,
-     ProfileResponse,
- } from './interface/profile.interface';
+export { CreateProfile, ProfileResponse } from "./interface/profile.interface";
 
 // Common
 
 // Helpers
-export { assertEnum } from './common/helpers/enum';
-export { isEnum } from './common/helpers/enum';
-export { parseEnum } from './common/helpers/enum';
-export { stringToEnum } from './common/helpers/enum';
+export { assertEnum } from "./common/helpers/enum";
+export { isEnum } from "./common/helpers/enum";
+export { parseEnum } from "./common/helpers/enum";
+export { stringToEnum } from "./common/helpers/enum";
 
 // Constants
-export { PaginationDefault } from './common/consts';
-export { SortDefault } from './common/consts';
+export { PaginationDefault } from "./common/consts";
+export { SortDefault } from "./common/consts";
 
 // Types
 
 // Data
-export { DomainLabel } from './common/types/data.types';
-export { TechnicalLabel } from './common/types/data.types';
+export { DomainLabel } from "./common/types/data.types";
+export { TechnicalLabel } from "./common/types/data.types";
 
 // Project
-export { Goal } from './common/types/project.types';
-export { ProjectStatus } from './common/types/project.types';
-export { JoinRequestStatus } from './common/types/project.types';
+export { Goal } from "./common/types/project.types";
+export { ProjectStatus } from "./common/types/project.types";
+export { JoinRequestStatus } from "./common/types/project.types";
+export { Project } from "./common/types/project.types";
 
 // Language
-export { LanguageCode } from './common/types/language.types';
-export { LanguageName } from './common/types/language.types';
-export { Language } from './common/types/language.types';
+export { LanguageCode } from "./common/types/language.types";
+export { LanguageName } from "./common/types/language.types";
+export { Language } from "./common/types/language.types";
 
 // User
-export { User } from './common/types/user.types';
-export { UserRole } from './common/types/user.types';
+export { User } from "./common/types/user.types";
+export { UserRole } from "./common/types/user.types";
 
 // Notification
-export { NotificationType } from './common/types/notification.types';
-
+export { NotificationType } from "./common/types/notification.types";
