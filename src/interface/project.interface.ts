c93cf8562c8feec64a3109e8af21e88e435a6ef8
Vadim Kim <vkim0203@gmail.com>
2025-08-06 16:23:26 +0900

    fix: type change according to the backend response

--- src/interface/project.interface.ts
+++ src/interface/project.interface.ts
@@ -1,94 +1,94 @@
-import { Goal, ProjectStatus } from "../common/types/project.types";
-import { Language, LanguageCode } from "../common/types/language.types";
-import { User } from "../common/types/user.types";
+import { Goal, ProjectStatus } from '../common/types/project.types';
+import { Language, LanguageCode } from '../common/types/language.types';
+import { User } from '../common/types/user.types';
 
 export interface CreateProject {
-    founderId: number;
-    title: string;
-    description?: string;
-    technicalLabels?: string[];
-    domainLabels?: string[];
-    goal: Goal;
-    status: ProjectStatus;
-    languageCode: LanguageCode;
-    milestone?: Date;
+  founderId: number;
+  title: string;
+  description?: string;
+  technicalLabels?: string[];
+  domainLabels?: string[];
+  goal: Goal;
+  status: ProjectStatus;
+  languageCode: LanguageCode;
+  milestone?: Date;
 }
 
 export interface UpdateProject {
-    founderId: number;
-    id: number;
-    title: string;
-    description: string;
-    technicalLabels: string[];
-    domainLabels: string[];
-    status: ProjectStatus;
-    languageCode: LanguageCode;
-    milestone: Date;
-    goal?: Goal;
+  founderId: number;
+  id: number;
+  title: string;
+  description: string;
+  technicalLabels: string[];
+  domainLabels: string[];
+  status: ProjectStatus;
+  languageCode: LanguageCode;
+  milestone: Date;
+  goal?: Goal;
 }
 
 export interface ProjectResponse {
-    id: number;
-    title: string;
-    description?: string;
-    technicalLabels?: string[];
-    domainLabels?: string[];
-    goal: Goal;
-    status: ProjectStatus;
-    language: Language;
-    milestone?: Date;
-    createdAt: Date;
-    lastUpdatedAt: Date;
-    users: User[];
-    founder: User;
-    joinRequest?: User[];
-    savedByUsers?: User[];
+  id: number;
+  title: string;
+  description?: string;
+  technicalLabels?: Array<{ projectId: number; labelName: string }>;
+  domainLabels?: Array<{ projectId: number; labelName: string }>;
+  goal: Goal;
+  status: ProjectStatus;
+  language: Language;
+  milestone?: Date;
+  createdAt: Date;
+  lastUpdatedAt: Date;
+  users: User[];
+  founder: User;
+  joinRequest?: User[];
+  savedByUsers?: User[];
 }
 
 export interface GetProject {
-    id: number;
+  id: number;
 }
 
 export interface SearchProject {
-    title?: string;
-    description?: string;
-    technicalLabels?: string;
-    domainLabels?: string;
-    status?: ProjectStatus;
-    languageCode?: LanguageCode;
-    goal?: Goal;
-    milestoneFrom?: Date;
-    milestoneTo?: Date;
-    createdAtFrom?: Date;
-    createdAtTo?: Date;
-    lastUpdatedAtFrom?: Date;
-    lastUpdatedAtTo?: Date;
-    page?: number;
-    limit?: number;
-    sort?: string;
+  title?: string;
+  description?: string;
+  technicalLabels?: string;
+  domainLabels?: string;
+  status?: ProjectStatus;
+  languageCode?: LanguageCode;
+  goal?: Goal;
+  milestoneFrom?: Date;
+  milestoneTo?: Date;
+  createdAtFrom?: Date;
+  createdAtTo?: Date;
+  lastUpdatedAtFrom?: Date;
+  lastUpdatedAtTo?: Date;
+  page?: number;
+  limit?: number;
+  sort?: string;
 }
 
 export interface SearchProjectResponse {
-    projects: ProjectResponse[];
-    page: number;
-    totalPages: number;
-    totalItems: number;
-    limit: number;
+  projects: ProjectResponse[];
+  page: number;
+  totalPages: number;
+  totalItems: number;
+  limit: number;
 }
 
 export interface JoinRequestResponse {
-    userId: number;
-    projectId: number;
-    roleName: string;
-    status: string;
-    message?: string;
+  userId: number;
+  projectId: number;
+  roleName: string;
+  status: string;
+  message?: string;
 }
 
 export interface CreateJoinRequest {
-    roleName: string;
-    message?: string;
+  roleName: string;
+  message?: string;
 }
 
 export interface Saveproject {
-    saved_by_users?: number[];
+  saved_by_users?: number[];
 }
