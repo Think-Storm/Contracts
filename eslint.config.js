e6b4f30f1e9981dcfdea3ea36908c9108f6cafeb
Vadim Kim <vkim0203@gmail.com>
2025-08-06 18:02:09 +0900

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
 export default [
   {
     files: ['**/*.ts'],
     languageOptions: {
-      parser: '@typescript-eslint/parser',
+      parser: parser,
     },
     plugins: {
       '@typescript-eslint': tseslint,
